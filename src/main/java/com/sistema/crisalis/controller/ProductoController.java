f09f8d3c113567487f03b4b530078b885ba89c00
Diego Villalba <diegoo.villalba@gmail.com>
2022-04-03 19:07:46 -0300

    Anadimos controlador para mostrar pagina de productos

---
+++ src/main/java/com/sistema/crisalis/controller/ProductoController.java
@@ -0,0 +1,18 @@
+package com.sistema.crisalis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/productos")
+public class ProductoController {
+	
+	@GetMapping("")
+	public String showProductos() {
+		
+		return "producto/show";
+	}
+	
+
+}
--- src/main/java/com/sistema/crisalis/servicesImpl/ProductoServiceImpl.java
+++ src/main/java/com/sistema/crisalis/servicesImpl/ProductoServiceImpl.java
@@ -9,17 +9,18 @@ import com.sistema.crisalis.model.Producto;
 import com.sistema.crisalis.repository.ProductoDAO;
 import com.sistema.crisalis.service.ProductoService;
 
-@Service //Anotacion Service: nos permite inyectar en el controlador la clase y hacer el llamado a los metodos
-public class ProductoServiceImpl implements ProductoService{
-	
-	/*Objeto Repository:
-	 * La interfaz ProductoDAO del paquete repository, esta interfaz obtiene todos los metodos CRUD
-	 * que nos proprociona JpaRepository  */
-	
-	@Autowired //Inyectamos la dependencia
+@Service // Anotacion Service: nos permite inyectar en el controlador la clase y hacer el
+			// llamado a los metodos
+public class ProductoServiceImpl implements ProductoService {
+
+	/*
+	 * Objeto Repository: La interfaz ProductoDAO del paquete repository obtiene
+	 * todos los metodos CRUD (save, delete, find) que nos proprociona JpaRepository
+	 */
+
+	@Autowired // Inyectamos la dependencia
 	private ProductoDAO productoDAO;
-	
-	
+
 	@Override
 	public Producto insertar(Producto producto) {
 		return productoDAO.save(producto);
@@ -32,14 +33,13 @@ public class ProductoServiceImpl implements ProductoService{
 
 	@Override
 	public void update(Producto producto) {
-		productoDAO.save(producto); //Si el objeto ya existe en la BBDD lo actualzia, por eso usamos "save" tambien
-		
+		productoDAO.save(producto); // Si el objeto ya existe en la BBDD lo actualzia, por eso usamos "save" tambien
+
 	}
 
 	@Override
 	public void delete(Integer id) {
 		productoDAO.deleteById(id);
 	}
-	
 
 }
